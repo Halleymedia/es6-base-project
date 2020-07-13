9d24f4175061ebb527f04c08a12939f3323056e9
Moreno Gentili <gentili@halleymedia.com>
2020-07-13 11:36:31 +0200

    Added support for async tests

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,8 @@ module.exports = {
     testEnvironment: "node",
     transform: {
       "\\.js$": ["babel-jest", { configFile: path.resolve(__dirname, './babel.config.js') }]
-    }
+    },
+    setupFiles: [
+      path.resolve(__dirname, "./jest.init.js")
+    ]
 };
